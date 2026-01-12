72555da657af5c16387278df56c9861917297d02
Abhishek <imaadidikshit@gmail.com>
2026-01-12 10:50:45 +0000

    Updated Fetching Time

--- frontend/public/embed.js
+++ frontend/public/embed.js
@@ -260,7 +260,7 @@
 
                 var safeFallback = "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23cbd5e1'%3E%3Cpath d='M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z' /%3E%3C/svg%3E";
                 var avatarUrl = item.respondent_photo_url;
-                if (!avatarUrl) { avatarUrl = 'https://ui-avatars.com/api/?background=random&color=fff&name=' + encodeURIComponent(item.respondent_name); }
+                if (!avatarUrl) { avatarUrl = 'https://ui-avatars.com/api/?background=random&color=fff&name=' + encodeURIComponent(item.respondent_name[0]); }
 
                 var card = document.createElement('div');
                 var isDark = theme === 'dark'; 
